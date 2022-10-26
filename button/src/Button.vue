6c2bb1e7f090d9ecb3ff6c0e35d833f077622d88
Christo <4911236+undefined@users.noreply.github.com>
2022-10-26 14:47:44 +0000

    New base file ready

--- button/src/Button.vue
+++ button/src/Button.vue
@@ -1,58 +1,18 @@
 <template>
-  <button :class="variant">
-    <slot></slot>
-  </button>
+  <button class="primary">Hi</button>
 </template>
-
-<script setup lang="ts">
-import { PropType } from 'vue';
-
-const props = defineProps({
-  /**
-   * The variant of the button - either 'primary' or 'secondary'
-   * @example 'secondary'
-   */
-  variant: {
-    type: String as PropType<'primary' | 'secondary'>,
-    default: 'primary',
-    validator: (value: any) => ['primary', 'secondary'].includes(value),
-  },
-});
-</script>
 
 <style lang="scss" scoped>
 @import '../../tokens/variables.scss';
 
 button {
-  padding: var(--vue-spacing-00) var(--vue-spacing-2);
-  border-radius: var(--vue-border-radius-pill);
-  border: 1px solid var(--vue-color-border);
-  font-family: var(--vue-font-family);
-  font-size: var(--vue-font-size-00);
-  font-weight: var(--vue-font-weight-button);
-  letter-spacing: var(--vue-letter-spacing-button);
+  padding: 16px 24px;
+  border-radius: 8px;
+  border: 1px solid $figma-global-button-primary-background;
   text-transform: uppercase;
 
   &.primary {
-    color: var(--vue-color-text-on-primary);
     background-color: $figma-global-button-primary-background;
-  }
-
-  &.secondary {
-    background-color: var(--vue-color-secondary);
-    color: var(--vue-color-text-on-secondary);
-  }
-
-  &.disabled,
-  &:disabled {
-    background-color: transparent;
-    pointer-events: none;
-    filter: brightness(50%);
-  }
-
-  &:hover {
-    color: var(--vue-color-text-on-primary);
-    filter: brightness(1.1);
   }
 }
 </style>
