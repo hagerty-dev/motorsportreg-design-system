ebc22a7705dad08eb0626400f56065663043448c
ChristoSteyn <christo@bare.bz>
2022-12-14 17:25:46 +0200

    doc: Add border width and radius  docs

---
+++ border/src/_border.js
@@ -0,0 +1,14 @@
+export const border = [
+  { token: 'none', value: '0px' },
+  { token: '100', value: '3px' },
+  { token: '200', value: '8px' },
+  { token: '300', value: '16px' },
+  { token: '400', value: '32px' },
+  { token: 'circle', value: '1000px' },
+  { token: 'pill', value: '9999px' },
+];
+
+export const borderWidth = [
+  { token: 100, value: '1px' },
+  { token: 200, value: '2px' },
+];
---
+++ border/src/msrDocBorder.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="msr-doc-table">
+    <table class="msr-doc-swatch">
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Value</th>
+          <th>Preview</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="border in data">
+          <td>{{ border.token }}</td>
+          <td>
+            {{ border.value }}
+          </td>
+          <td>
+            <div
+              v-if="border.token === 'circle'"
+              class="msr-doc-swatch__sample"
+              :style="{ borderRadius: border.value, width: '72px' }"
+            ></div>
+            <div
+              v-else
+              class="msr-doc-swatch__sample"
+              :style="{ borderRadius: border.value, width: '100px' }"
+            ></div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: {},
+    },
+    checker: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.msr-doc-swatch {
+  font-size: var(--dockit-swatch-title);
+  &__sample {
+    background-color: var(--msr-color-gray-300);
+    height: 72px;
+    display: block;
+  }
+}
+</style>
